2f76d395c35b1e8addd86800e84d714f75ff4149
zwei <wzweizhe@gmail.com>
2015-03-19 15:45:53 +0100

    UI improvement and add pagination for questions
    pagination with angular-ui bootstrap

--- src/main/webapp/resources/js/qmcController.js
+++ src/main/webapp/resources/js/qmcController.js
@@ -1,12 +1,21 @@
 angular.module("qmcApp")
 .controller("qmcController", function ($scope, $http, $resource, baseUrl, getAnswerUrl, data) {
 
-	$scope.questionsResource = $resource(baseUrl + ":id", {id : "@id"});
-	$scope.questionsWithCorrectAnswerResource = $resource(getAnswerUrl + ":id", {id : "@id"});
+//	$scope.questionsResource = $resource(baseUrl + ":id", {id : "@id"});
+//	$scope.questionsWithCorrectAnswerResource = $resource(getAnswerUrl + ":id", {id : "@id"});
+	
+	//pagination setting
+	$scope.totalItems = 0;  //Total number of items in all pages.
+	$scope.currentPage = 1;
+	$scope.maxPaginationSize = 3;   //Limit number for pagination size.
+	$scope.itemsPerPage = 1;
+	$scope.currentQuestion = function(){ return $scope.questions[$scope.currentPage-1]};
+	$scope.currentChoices = function(){ return $scope.currentQuestion().choices};
 	
 	$scope.findAll = function(){
 //		$scope.questions = $scope.questionsResource.query();
 		$scope.questions = data;
+		$scope.totalItems = $scope.questions.length;
 	}
 	
 	$scope.sendChoices = function(){
@@ -27,42 +36,42 @@ angular.module("qmcApp")
 		);
 	}
 	
-	$scope.getCorrectChoices = function(){
-		$scope.questionsWithCorrectAnswerAccessor = $scope.questionsWithCorrectAnswerResource.query(); //the result of this query() is an empty array, which is populated later.
-		$scope.questionsWithCorrectAnswerAccessor.$promise.then(  //when the ajax query return and it's success, the argument is callback function that will be called.
-				function(data){
-					$scope.questionsWithCorrectAnswer = data;
-					$scope.updateWithCorrectChoices();
-				}
-		);
-	}
+//	$scope.getCorrectChoices = function(){
+//		$scope.questionsWithCorrectAnswerAccessor = $scope.questionsWithCorrectAnswerResource.query(); //the result of this query() is an empty array, which is populated later.
+//		$scope.questionsWithCorrectAnswerAccessor.$promise.then(  //when the ajax query return and it's success, the argument is callback function that will be called.
+//				function(data){
+//					$scope.questionsWithCorrectAnswer = data;
+//					$scope.updateWithCorrectChoices();
+//				}
+//		);
+//	}
 		
-	$scope.updateWithCorrectChoices = function(){
-		$scope.goodChoiceNum = 0;
-		$scope.badChoiceNum = 0;
-		$scope.wrongAnswer = 0;
-		
-		var questions = $scope.questions;
-		for(i=0; i<questions.length; i++){
-			var wrongAnswer = 0;
-			
-			for(j=0; j<questions[i].choices.length; j++){
-				var currentChoice = questions[i].choices[j];
-				var answerChoice = $scope.questionsWithCorrectAnswer[i].choices[j];
-				currentChoice.correctness = answerChoice.correctness;
-				if(((currentChoice.selected==true) && (currentChoice.correctness==true))
-					|| 	((currentChoice.selected==false) && (currentChoice.correctness==false)) ){
-					$scope.goodChoiceNum += 1;
-				} else {
-					$scope.badChoiceNum += 1;
-					wrongAnswer += 1;
-				}
-			}
-			if(wrongAnswer > 0){
-				$scope.wrongAnswer += 1;
-			}
-		}
-	}
+//	$scope.updateWithCorrectChoices = function(){
+//		$scope.goodChoiceNum = 0;
+//		$scope.badChoiceNum = 0;
+//		$scope.wrongAnswer = 0;
+//		
+//		var questions = $scope.questions;
+//		for(i=0; i<questions.length; i++){
+//			var wrongAnswer = 0;
+//			
+//			for(j=0; j<questions[i].choices.length; j++){
+//				var currentChoice = questions[i].choices[j];
+//				var answerChoice = $scope.questionsWithCorrectAnswer[i].choices[j];
+//				currentChoice.correctness = answerChoice.correctness;
+//				if(((currentChoice.selected==true) && (currentChoice.correctness==true))
+//					|| 	((currentChoice.selected==false) && (currentChoice.correctness==false)) ){
+//					$scope.goodChoiceNum += 1;
+//				} else {
+//					$scope.badChoiceNum += 1;
+//					wrongAnswer += 1;
+//				}
+//			}
+//			if(wrongAnswer > 0){
+//				$scope.wrongAnswer += 1;
+//			}
+//		}
+//	}
 	
 	$scope.findAll();
 });
